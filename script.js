eedd0c91a667f11ec9935152846d455bbc9a738c
Rita Dewittsmith <sdewittsmith1@gmail.com>
2022-04-19 21:38:42 -0400

    class6 project

---
+++ script.js
@@ -0,0 +1,17 @@
+
+ let getdog = prompt("Do you Own a dog?");
+if (getdog === "yes") {
+    document.write("I own a dog too!");
+} else {
+    document.write("this may not be the page for you");
+}
+
+let dogname = prompt("what is your dog's name?");
+document.write("very cool name!")
+
+let dogage = prompt("what is your dog's Age?");
+document.write("awww!")
+
+let dogbreed = prompt("what is your dog's breed?");
+document.write("awww!")
+
